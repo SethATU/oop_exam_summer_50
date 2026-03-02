63f84bc4b446c118b0eb5ce4018bf97101a77b9e
SethButler-STUDENT <g00467621@atu.ie>
2026-03-02 12:20:48 +0000

    added errors to controller

--- src/main/java/ie/atu/oop_exam_summer_50/controller/BookingController.java
+++ src/main/java/ie/atu/oop_exam_summer_50/controller/BookingController.java
@@ -17,20 +17,46 @@ public class BookingController {
 
     @GetMapping("/id")
     public ResponseEntity<?> reservationId(@RequestParam int id) {
-        int identity = bookingService.reservationId(id);
-        return ResponseEntity.ok("User id: " + identity);
+        if(id < 0) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("id cant be a negative number");
+        }
+        else {
+            int identity = bookingService.reservationId(id);
+            return ResponseEntity.ok("User id: " + identity);
+        }
     }
 
     @GetMapping("/tag")
     public ResponseEntity<?> reservationTag(@RequestParam int tag) {
-        int TAG = bookingService.equipmentTag(tag);
-        return ResponseEntity.ok("Equipment Tag: " + TAG);
+        if(tag > 999) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Tag cant be larger than 3 digits");
+        }
+        else if(tag < 0) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Tag cant be negative");
+        }
+        else {
+            int TAG = bookingService.equipmentTag(tag);
+            return ResponseEntity.ok("Equipment Tag: " + TAG);
+        }
     }
 
     @GetMapping("/email")
     public ResponseEntity<?> studentEmail(@RequestParam String email) {
-        String Email = bookingService.studentEmail(email);
-        return ResponseEntity.ok("Student Email: " + Email);
+        if(email.contains("@atu.ie")) {
+            String Email = bookingService.studentEmail(email);
+            return ResponseEntity.ok("Student Email: " + Email);
+        }
+        else {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Student Email must end with @atu.ie");
+        }
     }
 
     @GetMapping("/date")
@@ -40,14 +66,28 @@ public class BookingController {
     }
     @GetMapping("/start")
     public ResponseEntity<?> startHour(@RequestParam int hour) {
-        int HOUR = bookingService.startHour(hour);
-        return ResponseEntity.ok("Start Hour: " + HOUR);
+        if(hour < 0 || hour > 23) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Hour must be between 0 and 23");
+        }
+        else {
+            int HOUR = bookingService.startHour(hour);
+            return ResponseEntity.ok("Start Hour: " + HOUR);
+        }
     }
 
     @GetMapping("/duration")
     public ResponseEntity<?> durationHour(@RequestParam int duration) {
-        int DURATION = bookingService.durationHours(duration);
-        return ResponseEntity.ok("Duration: " + DURATION);
+        if(duration < 0 || duration > 59) {
+            int DURATION = bookingService.durationHours(duration);
+            return ResponseEntity.ok("Duration: " + DURATION);
+        }
+        else {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Duration must be between 0 and 59 days");
+        }
     }
 
 
